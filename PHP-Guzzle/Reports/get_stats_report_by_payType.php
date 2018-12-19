8c37a86d4a9fa16c49eb4dad1654ef4ad89db88c
Hayden Lingle <hayden.lingle@gmail.com>
2018-12-19 13:59:56 -0700

    Added remaining php report examples.

---
+++ PHP-Guzzle/Reports/get_stats_report_by_payType.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Stats Report (By Pay Type)</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <h1>Get Statistics Report (By Payment Type)</h1>
+  <?php 
+
+    // --- REQUIRES GUZZLE --- 
+    // http://docs.guzzlephp.org/en/latest/overview.html#installation
+
+    require 'vendor/autoload.php';
+    use GuzzleHttp\Client;
+    use GuzzleHttp\Psr7;
+    use GuzzleHttp\ExceptionRequestException;
+
+    $client = new Client([
+      'base_uri' => 'https://secure.petexec.net/api/',
+      'timeout' => 2.0,
+    ]);
+
+    try {
+      $client_id = ''; // Enter your client id
+      $client_secret = ''; // Public apps should not expose client secret
+
+      $base64_client = base64_encode($client_id . ':' . $client_secret);
+
+      $username = ''; // Enter your username
+      $password = ''; // Enter your password
+
+      $startDate = ''; // yyyy-mm-dd
+      $endDate = ''; // yyyy-mm-dd
+      $payType = ''; // Cash, Check, Credit, Clover Mini, Tradeout, Refund, No Payment, Gift Certificate, Paw Point Redemption, Not Processed, Discount, Tax, Tip / Gratuity
+
+      $params = array(
+        'username' => $username,
+        'password' => $password,
+        'grant_type' => 'password',
+        'scope' => 'report_read' // Enter desired scopes in space separated list
+      );
+
+      $headers = array(
+        'Authorization' => 'Basic ' . $base64_client,
+      );
+
+      // Retrieve an access token
+      $response = $client->post('token', ['form_params' => $params, 'headers' => $headers]);
+      $data = json_decode($response->getBody()->__toString(), true);
+      $access_token = $data['access_token'];
+
+      // Retrieve statistics report
+      $custom_endpoint = 'report/statistics-report/' . $startDate . '/' . $endDate . '/breakdown/' . $payType;
+      $get_response = $client->request('get', $custom_endpoint, ['headers' => ['Authorization' => 'Bearer ' . $access_token]]);
+      $body = $get_response->getBody();
+      echo $body;
+    } catch(RequestException $e) {
+      if ($e->hasResponse()) {
+        echo Psr7str($e->getResponse());
+      }
+    }
+  ?>
+</body>
+</html>
